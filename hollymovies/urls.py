205b45f1e9cbbcd9d2c377996d0c6af0e9933abe
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-02-15 14:50:01 +0100

    countries

--- hollymovies/urls.py
+++ hollymovies/urls.py
@@ -1,37 +1,35 @@
-"""
-URL configuration for hollymovies project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
-from viewer.views import movies, home, movie, genres, genre, GenresView, \
-    GenresTemplateView, GenresListView, CreatorsListView, CreatorView
+from viewer.views import (
+    CreatorsListView,
+    CreatorView,
+    GenresListView,
+    GenresTemplateView,
+    GenresView,
+    countries,
+    genre,
+    genres,
+    home,
+    movie,
+    movies,
+    one_country,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('', home, name='home'),
-
-    path('movies/', movies, name='movies'),
-    path('movie/<int:pk>/', movie, name='movie'),
-    #path('genres/', genres, name='genres'),  # view pomocí funkce genres
-    #path('genres/', GenresView.as_view(), name='genres'),  # view pomocí třídy GenresView
-    #path('genres/', GenresTemplateView.as_view(), name='genres'),  # view pomocí třídy GenresTemplateView
-    path('genres/', GenresListView.as_view(), name='genres'),  # view pomocí třídy GenresListView
-    path('genre/<int:pk>/', genre, name='genre'),
-    path('creators/', CreatorsListView.as_view(), name='creators'),
-    path('creator/<int:pk>/', CreatorView.as_view(), name='creator'),
+    path("admin/", admin.site.urls),
+    path("", home, name="home"),
+    path("movies/", movies, name="movies"),
+    path("movie/<int:pk>/", movie, name="movie"),
+    # path('genres/', genres, name='genres'),  # view pomocí funkce genres
+    # path('genres/', GenresView.as_view(), name='genres'),  # view pomocí třídy GenresView
+    # path('genres/', GenresTemplateView.as_view(), name='genres'),  # view pomocí třídy GenresTemplateView
+    path(
+        "genres/", GenresListView.as_view(), name="genres"
+    ),  # view pomocí třídy GenresListView
+    path("genre/<int:pk>/", genre, name="genre"),
+    path("creators/", CreatorsListView.as_view(), name="creators"),
+    path("creator/<int:pk>/", CreatorView.as_view(), name="creator"),
+    path("countries/", countries, name="country_list"),
+    path("country/<int:pk>/", one_country, name="one_country"),
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView, TemplateView
 
-from viewer.models import Movie, Genre, Creator
+from viewer.models import Country, Creator, Genre, Movie
 
 
 def home(request):
@@ -11,36 +12,50 @@ def home(request):
 
 def movies(request):
     movies_ = Movie.objects.all()
-    context = {'movies': movies_}
-    return render(request=request,
-                  template_name="movies.html",
-                  context=context)
+    context = {"movies": movies_}
+    return render(request=request, template_name="movies.html", context=context)
 
 
 def movie(request, pk):
     if Movie.objects.filter(id=pk).exists():
         movie_ = Movie.objects.get(id=pk)
-        context = {'movie': movie_}
+        context = {"movie": movie_}
         return render(request, "movie.html", context)
     return redirect("home")
 
 
 def genres(request):
-    return render(request,
-                  "genres.html",
-                  {'genres': Genre.objects.all()})
+    return render(
+        request,
+        template_name="genres.html",
+        context={"genres": Genre.objects.all()},
+    )
+
+
+def countries(request):
+    context = {"countries": Country.objects.all()}
+    return render(
+        request=request, template_name="countries.html", context=context
+    )
+
+
+def one_country(request, pk: int):
+    if Country.objects.filter(id=pk).exists():
+        context = {"country": Country.objects.get(id=pk)}
+        return render(
+            request=request, template_name="country.html", context=context
+        )
+    return HttpResponse("Error")
 
 
 class GenresView(View):
     def get(self, request):
-        return render(request,
-                      "genres.html",
-                      {'genres': Genre.objects.all()})
+        return render(request, "genres.html", {"genres": Genre.objects.all()})
 
 
 class GenresTemplateView(TemplateView):
     template_name = "genres.html"
-    extra_context = {'genres': Genre.objects.all()}
+    extra_context = {"genres": Genre.objects.all()}
 
 
 class GenresListView(ListView):
@@ -48,25 +63,27 @@ class GenresListView(ListView):
     model = Genre
     # pozor, do template se posílají data pod názvem 'object_list'
     # nebo můžu přejmenovat
-    context_object_name = 'genres'
+    context_object_name = "genres"
 
 
 def genre(request, pk):
     if Genre.objects.filter(id=pk).exists():
-        return render(request,
-                      "genre.html",
-                      {'genre': Genre.objects.get(id=pk)})
-    return redirect('genres')
+        return render(
+            request, "genre.html", {"genre": Genre.objects.get(id=pk)}
+        )
+    return redirect("genres")
 
 
 class CreatorsListView(ListView):
     template_name = "creators.html"
     model = Creator
-    context_object_name = 'creators'
+    context_object_name = "creators"
 
 
 class CreatorView(View):
     def get(self, request, pk):
         if Creator.objects.filter(id=pk).exists():
-            return render(request, "creator.html", {"creator": Creator.objects.get(id=pk)})
-        return redirect('creators')
+            return render(
+                request, "creator.html", {"creator": Creator.objects.get(id=pk)}
+            )
+        return redirect("creators")
